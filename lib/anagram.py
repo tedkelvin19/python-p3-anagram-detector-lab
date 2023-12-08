74df564d092bfba1286ff962dd30ada329ed4bb8
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-12-08 13:50:49 +0300

    Completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, words):
+        return [w for w in words if self.word != w and sorted(self.word) == sorted(w)]
+    
+listen = Anagram("listen")
+print(listen.match(["enlist", "google", "inlets", "banana"]))
